3e7ea1ed1533cb22e8859b4b2df2fefe833a708d
lujianfeiccie <lujianfeiccie@gmail.com>
2014-01-01 18:50:48 +0800

    add thread process

--- src/com/example/citylistview/MainActivity.java
+++ src/com/example/citylistview/MainActivity.java
@@ -6,7 +6,9 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.citylistview.CityListView.ChineseCharComp;
 
@@ -16,6 +18,7 @@ public class MainActivity extends Activity {
 	private LinearLayout mLinearLayout;
 	private List<String> data; //城市名称
 	private DBManager dbmanager = null;
+	private TextView txt_loading = null;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
@@ -27,19 +30,33 @@ public class MainActivity extends Activity {
 		dbmanager = new DBManager(this);
 		mListView = (CityListView)findViewById(R.id.lv_citylist);
 		mLinearLayout = (LinearLayout) findViewById(R.id.ll_indicator);
+		txt_loading = (TextView)findViewById(R.id.txt_loading);
 	}
 	
 	public void initData() {
-		initCityData();
-		Collections.sort(this.data, new ChineseCharComp());
-		data.add(0, "root定位城市");
-		data.add(1, "sub深圳市");
-		data.add(2,"root热门城市");
-		data.add(3,"sub北京市");
-		data.add(4,"sub上海市");
-		data.add(5,"sub广州市");
-		mListView.init(mLinearLayout,data);
+		txt_loading.setVisibility(View.VISIBLE);
+		new Thread()
+		{
+			public void run() {
+				initCityData();
+				Collections.sort(data, new ChineseCharComp());
+				data.add(0, "root定位城市");
+				data.add(1, "sub深圳市");
+				data.add(2,"root热门城市");
+				data.add(3,"sub北京市");
+				data.add(4,"sub上海市");
+				data.add(5,"sub广州市");
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						txt_loading.setVisibility(View.GONE);
+						mListView.init(mLinearLayout,data);
+					}
+				});
+			};
+		}.start();
 	}
+	
 	public void initEvent() {
 		//mListView.setOnItemClickListener(this);
 	}
